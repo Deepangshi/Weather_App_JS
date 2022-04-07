b46d9e9c249d4075ae17e54b246ec5700ef34850
Deepangshi <deepangshi@gmail.com>
2022-04-07 21:32:13 +0545

    initial commit/files

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let weather = {
-    "apiKey": "your-key-here",
+    "apiKey": "your-api-key-here",
     fetchWeather: function (city) {
         fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&appid=" + this.apiKey)
             .then((response) => response.json())
